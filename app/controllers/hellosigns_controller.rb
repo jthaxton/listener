03a75f087d528fe5b1c9fd54b703cec91987d254
Joe Thaxton <14jthaxton@gmail.com>
2019-05-18 13:48:56 -0700

    a

--- app/controllers/hellosigns_controller.rb
+++ app/controllers/hellosigns_controller.rb
@@ -1,4 +1,6 @@
 class HellosignsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def create
         Hellosign.new(payload: params.as_json).save!
     end 
